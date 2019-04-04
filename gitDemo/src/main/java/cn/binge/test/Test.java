aca226bd9f6d1dc1a72cf4b3c499f838a660720a
yanbin <1002456322@qq.com>
2019-04-04 10:11:51 +0800

    新增项目文件

---
+++ gitDemo/src/main/java/cn/binge/test/Test.java
@@ -0,0 +1,32 @@
+package cn.binge.test;
+
+/**
+ * @Auther: yanbin
+ * @version: v1.0
+ * @Date: 2019/4/4 09:49
+ * @Description:
+ */
+public class Test {
+    /**
+     * @Description:
+     * 用于测试注释
+     * @Date 9:58 2019/4/4
+     * @Param [a, b, s]
+     * @return void
+     **/
+    public void tester(int a,int b,String s){
+
+    }
+    /**
+     * @Author yanbin
+     * @Description:
+     *
+     * @Date 10:06 2019/4/4
+     * @Param [oldStr]
+     * @return java.lang.String
+     **/
+     public String test1(String oldStr){
+        String newStr = oldStr + "思密达";
+        return newStr;
+     }
+}
